fecc4faf4962a85c83d293774152813d93971809
Zain Sadaqat <83048208+zainsadaqat@users.noreply.github.com>
2021-08-05 16:26:39 +0500

    Develop desktop version for home page

---
+++ about-script.js
@@ -0,0 +1,13 @@
+const body = document.querySelector('boyd');
+
+const hamburger = document.querySelector('.mobile-navbar');
+const openMenuContainer = document.querySelector('article.mobile-menu');
+const closeMenuBtn = document.querySelector('.close-menu-btn');
+
+hamburger.addEventListener('click', () => {
+  openMenuContainer.classList.toggle('d-none');
+});
+
+closeMenuBtn.addEventListener('click', () => {
+  openMenuContainer.classList.toggle('d-none');
+});
